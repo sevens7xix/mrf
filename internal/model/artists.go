c40fe5f1cb9f0b8dc7448f8bb79a439bcddb76a6
Vicko Quizza <ordinaryvicko@gmail.com>
2023-01-13 16:43:08 -0500

    create artists model

---
+++ internal/model/artists.go
@@ -0,0 +1,39 @@
+package model
+
+type Artists struct {
+	Href     string `json:"href"`
+	Items    []Item `json:"items"`
+	Limit    int    `json:"limit"`
+	Next     string `json:"next"`
+	Offset   int    `json:"offset"`
+	Previous string `json:"previous"`
+	Total    int    `json:"total"`
+}
+
+type Item struct {
+	ExternalUrls ExternalUrls `json:"external_urls"`
+	Followers    Followers    `json:"followers"`
+	Genres       []string     `json:"genres"`
+	Href         string       `json:"href"`
+	ID           string       `json:"id"`
+	Images       []Image      `json:"images"`
+	Name         string       `json:"name"`
+	Popularity   int          `json:"popularity"`
+	Type         string       `json:"type"`
+	URI          string       `json:"uri"`
+}
+
+type ExternalUrls struct {
+	Spotify string `json:"spotify"`
+}
+
+type Image struct {
+	Height int    `json:"height"`
+	URL    string `json:"url"`
+	Width  int    `json:"width"`
+}
+
+type Followers struct {
+	Href  string `json:"href"`
+	Total int    `json:"total"`
+}
