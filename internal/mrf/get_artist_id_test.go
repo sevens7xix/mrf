ec3ce53ccb34e0475981e8ae93f992337a4d7bd8
Vicko Quizza <ordinaryvicko@gmail.com>
2023-01-16 15:28:48 -0500

    finished the get artist_id methods tests

--- internal/mrf/get_artist_id_test.go
+++ internal/mrf/get_artist_id_test.go
@@ -2,7 +2,7 @@ package mrf
 
 import (
 	"bytes"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"testing"
 
@@ -13,7 +13,7 @@ func TestGetBearerToken(t *testing.T) {
 	client := NewTestClient(func(req *http.Request) *http.Response {
 		return &http.Response{
 			StatusCode: 200,
-			Body:       ioutil.NopCloser(bytes.NewBufferString(`{"access_token":"Getting credentials..."}`)),
+			Body:       io.NopCloser(bytes.NewBufferString(`{"access_token":"Getting credentials..."}`)),
 			Header:     make(http.Header),
 		}
 	})
@@ -25,4 +25,66 @@ func TestGetBearerToken(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, response)
 	assert.Equal(t, "Getting credentials...", response)
+}
+
+func TestGetArtistIdHappyPath(t *testing.T) {
+	client := NewTestClient(func(req *http.Request) *http.Response {
+		return &http.Response{
+			StatusCode: 200,
+			Body: io.NopCloser(bytes.NewBufferString(`{
+				"artists": {
+				  "href": "https://api.spotify.com/v1/search?query=michael+galaso&type=artist&locale=en-US%2Cen%3Bq%3D0.5&offset=0&limit=20",
+				  "items": [
+					{
+					  "href": "https://api.spotify.com/v1/artists/0zwktRkdtjFn2F8v9kUdlF",
+					  "id": "0zwktRkdtjFn2F8v9kUdlF",
+					  "name": "Michael Galasso",
+					  "uri": "spotify:artist:0zwktRkdtjFn2F8v9kUdlF"
+					}
+				  ],
+				  "limit": 20,
+				  "next": "https://api.spotify.com/v1/search?query=michael+galaso&type=artist&locale=en-US%2Cen%3Bq%3D0.5&offset=20&limit=20",
+				  "offset": 0,
+				  "previous": null,
+				  "total": 36
+				}
+			  }`)),
+			Header: make(http.Header),
+		}
+	})
+
+	service := NewService(client)
+
+	response, err := service.GetArtistID([]string{"Michael", "Galaso"})
+
+	assert.NoError(t, err)
+	assert.NotNil(t, response)
+	assert.Equal(t, "https://api.spotify.com/v1/artists/0zwktRkdtjFn2F8v9kUdlF", response)
+}
+
+func TestGetArtistIdUnhappyPath(t *testing.T) {
+	client := NewTestClient(func(req *http.Request) *http.Response {
+		return &http.Response{
+			StatusCode: 200,
+			Body: io.NopCloser(bytes.NewBufferString(`{
+				"artists": {
+				  "href": "https://api.spotify.com/v1/search?query=michael+galaso&type=artist&locale=en-US%2Cen%3Bq%3D0.5&offset=0&limit=20",
+				  "items": [],
+				  "limit": 20,
+				  "next": "https://api.spotify.com/v1/search?query=michael+galaso&type=artist&locale=en-US%2Cen%3Bq%3D0.5&offset=20&limit=20",
+				  "offset": 0,
+				  "previous": null,
+				  "total": 36
+				}
+			  }`)),
+			Header: make(http.Header),
+		}
+	})
+
+	service := NewService(client)
+
+	response, err := service.GetArtistID([]string{"Michael", "Galaso"})
+
+	assert.Error(t, err)
+	assert.Empty(t, response)
 }
