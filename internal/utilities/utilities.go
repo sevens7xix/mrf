286821b86ddda212d16b74d858b4447882c95ba1
Vicko Quizza <ordinaryvicko@gmail.com>
2023-01-17 15:43:24 -0500

    fix typo

--- internal/utilities/utilities.go
+++ internal/utilities/utilities.go
@@ -1,6 +1,10 @@
 package utilities
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/spf13/viper"
+)
 
 func StringFormatter(rawString []string) string {
 
@@ -9,4 +13,26 @@ func StringFormatter(rawString []string) string {
 	} else {
 		return rawString[0]
 	}
+}
+
+func GetCredentials() ([]string, error) {
+	// Here we're going to retreive the credentials on the .env file using viper
+	var credentials []string
+
+	viper.SetConfigName("credentials")
+	viper.SetConfigType("env")
+	viper.AddConfigPath("/home/saroui/Documents/mrf")
+	viper.AddConfigPath("/app")
+
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		return nil, err
+	}
+
+	// First we're going to get the client id, and later the client secret key
+	credentials = append(credentials, viper.GetString("CLIENT_ID"))
+	credentials = append(credentials, viper.GetString("CLIENT_SECRET"))
+
+	return credentials, nil
 }
