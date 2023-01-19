84550fdf9195e1d7eb1ae26120fc54f649348100
Vicko Quizza <ordinaryvicko@gmail.com>
2023-01-19 13:08:20 -0500

    fix duplicates in get artist test

--- internal/mrf/get_artist_id_test.go
+++ internal/mrf/get_artist_id_test.go
@@ -4,50 +4,18 @@ import (
 	"bytes"
 	"io"
 	"net/http"
+	"os"
 	"testing"
 
 	"github.com/sevens7xix/mrf/internal/utilities"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetBearerTokenHappyPath(t *testing.T) {
-	client := utilities.NewTestClient(func(req *http.Request) *http.Response {
-		return &http.Response{
-			StatusCode: 200,
-			Body:       io.NopCloser(bytes.NewBufferString(`{"access_token":"Getting credentials..."}`)),
-			Header:     make(http.Header),
-		}
-	})
+var client, badClient *http.Client
 
-	service := NewAlbumService([]string{"Phoenix"}, client)
+func TestMain(m *testing.M) {
 
-	response, err := service.GetBearerToken()
-
-	assert.NoError(t, err)
-	assert.NotNil(t, response)
-	assert.Equal(t, "Getting credentials...", response)
-}
-
-func TestGetBearerTokenUnHappyPath(t *testing.T) {
-	client := utilities.NewTestClient(func(req *http.Request) *http.Response {
-		return &http.Response{
-			StatusCode: 404,
-			Body:       io.NopCloser(bytes.NewBufferString(`{"error":"invalid_client"}`)),
-			Header:     make(http.Header),
-		}
-	})
-
-	service := NewAlbumService([]string{"Phoenix"}, client)
-
-	response, err := service.GetBearerToken()
-
-	assert.Error(t, err)
-	assert.Empty(t, response)
-}
-
-func TestGetArtistIdHappyPath(t *testing.T) {
-
-	client := utilities.NewTestClient(func(req *http.Request) *http.Response {
+	client = utilities.NewTestClient(func(req *http.Request) *http.Response {
 		if req.URL.String() == "https://accounts.spotify.com/api/token" {
 			return &http.Response{
 				StatusCode: 200,
@@ -80,6 +48,56 @@ func TestGetArtistIdHappyPath(t *testing.T) {
 		}
 	})
 
+	badClient = utilities.NewTestClient(func(req *http.Request) *http.Response {
+		if req.URL.String() == "https://accounts.spotify.com/api/token" {
+			return &http.Response{
+				StatusCode: 404,
+				Body:       io.NopCloser(bytes.NewBufferString(`{"error":"invalid_client"}`)),
+				Header:     make(http.Header),
+			}
+		} else {
+			return &http.Response{
+				StatusCode: 200,
+				Body: io.NopCloser(bytes.NewBufferString(`{
+					"artists": {
+					  "href": "https://api.spotify.com/v1/search?query=michael+galaso&type=artist&locale=en-US%2Cen%3Bq%3D0.5&offset=0&limit=20",
+					  "items": [],
+					  "limit": 20,
+					  "next": "https://api.spotify.com/v1/search?query=michael+galaso&type=artist&locale=en-US%2Cen%3Bq%3D0.5&offset=20&limit=20",
+					  "offset": 0,
+					  "previous": null,
+					  "total": 36
+					}
+				  }`)),
+				Header: make(http.Header),
+			}
+		}
+	})
+
+	code := m.Run()
+	os.Exit(code)
+}
+
+func TestGetBearerTokenHappyPath(t *testing.T) {
+	service := NewAlbumService([]string{"Phoenix"}, client)
+
+	response, err := service.GetBearerToken()
+
+	assert.NoError(t, err)
+	assert.NotNil(t, response)
+	assert.Equal(t, "Getting credentials...", response)
+}
+
+func TestGetBearerTokenUnHappyPath(t *testing.T) {
+	service := NewAlbumService([]string{"Phoenix"}, badClient)
+
+	response, err := service.GetBearerToken()
+
+	assert.Error(t, err)
+	assert.Empty(t, response)
+}
+
+func TestGetArtistIdHappyPath(t *testing.T) {
 	service := NewAlbumService([]string{"Michael", "Galasso"}, client)
 
 	response, err := service.GetArtistID()
@@ -90,25 +108,7 @@ func TestGetArtistIdHappyPath(t *testing.T) {
 }
 
 func TestGetArtistIdUnhappyPath(t *testing.T) {
-	client := utilities.NewTestClient(func(req *http.Request) *http.Response {
-		return &http.Response{
-			StatusCode: 200,
-			Body: io.NopCloser(bytes.NewBufferString(`{
-				"artists": {
-				  "href": "https://api.spotify.com/v1/search?query=michael+galaso&type=artist&locale=en-US%2Cen%3Bq%3D0.5&offset=0&limit=20",
-				  "items": [],
-				  "limit": 20,
-				  "next": "https://api.spotify.com/v1/search?query=michael+galaso&type=artist&locale=en-US%2Cen%3Bq%3D0.5&offset=20&limit=20",
-				  "offset": 0,
-				  "previous": null,
-				  "total": 36
-				}
-			  }`)),
-			Header: make(http.Header),
-		}
-	})
-
-	service := NewAlbumService([]string{"Mgwdfdfgjdn"}, client)
+	service := NewAlbumService([]string{"Mgwdfdfgjdn"}, badClient)
 
 	response, err := service.GetArtistID()
 
