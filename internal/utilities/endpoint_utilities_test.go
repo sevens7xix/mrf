739638ae6f5cf64e811a40bff04cadef9b1585b2
Vicko Quizza <ordinaryvicko@gmail.com>
2023-01-20 16:58:09 -0500

    add more utilites methods test

--- internal/utilities/endpoint_utilities_test.go
+++ internal/utilities/endpoint_utilities_test.go
@@ -1,6 +1,7 @@
 package utilities
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/sevens7xix/mrf/internal/model"
@@ -10,4 +11,15 @@ func TestProcessTitleTracks(t *testing.T) {
 	AssertEqual(t,
 		ProcessTitleTracks([]model.MockProcessedItem{}),
 		"=======================TRACKLIST=======================\n")
+}
+
+func TestPrintTracks(t *testing.T) {
+	buffer := bytes.Buffer{}
+
+	PrintTracks(&buffer, "a lot of tracks")
+
+	got := buffer.String()
+	wanted := "a lot of tracks"
+
+	AssertEqual(t, got, wanted)
 }
